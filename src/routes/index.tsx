ae5a50851a29b69512d0ff434be77bc25669ae4f
kassio oliveira <kassiooliveira875@gmail.com>
2023-03-07 16:11:28 -0300

    Adicionado tema escuro e troca de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import {Button} from '@mui/material'
+import {Button} from '@mui/material';
+import { useAppThemeContext } from '../shared/contexts/ThemeContext';
+
+
 
 export const AppRoutes = () => {
+
+  const {toggleTheme} = useAppThemeContext();
   return (
     <Routes>
-        <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' >teste</Button>} />
+        <Route path='/pagina-inicial' element={
+        <Button variant='contained'color='primary' onClick={toggleTheme} >toggle theme</Button>} />
 
         <Route path='*' element={<Navigate to='/pagina-inicial'/>} />
     </Routes>
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useCallback, useContext, useMemo, useState } from "react";
+import { ThemeProvider } from '@mui/material';
+import {Box} from '@mui/system';
+
+import { DarkTheme, LightTheme } from "../themes";
+
+interface IThemeContextData {
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;
+}
+
+const ThemContext = createContext({} as IThemeContextData);
+
+interface IChildrenProp {
+    children: React.ReactNode;
+}
+
+export const useAppThemeContext = () => {
+    return useContext(ThemContext);
+}
+
+export const AppThemeProvider: React.FC<IChildrenProp> = ({children}) => {
+    const [themeName,setThemeName] = useState<'light' | 'dark'>('light');
+    
+    const toggleTheme = useCallback(()=>{
+        setThemeName(oldTheme => oldTheme === 'light'? 'dark': 'light');
+
+    },[]);
+
+    const theme = useMemo(()=> {
+        if(themeName === 'light')return LightTheme;
+
+        return DarkTheme;
+
+    },[themeName]);
+
+    return (
+        <ThemContext.Provider value={{themeName, toggleTheme}}>
+             <ThemeProvider theme={theme}>
+             <Box width='100vw' height='100vh' bgcolor={theme.palette.background.default}>
+             {children}
+             </Box>
+             </ThemeProvider>
+        </ThemContext.Provider>
+    )
+}
