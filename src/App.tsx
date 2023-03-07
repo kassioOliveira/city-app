a9355d641fb9e0d5ee700fd2f0e744c5acc3bda6
kassio oliveira <kassiooliveira875@gmail.com>
2023-03-07 15:04:16 -0300

    Adicionando tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from '@mui/material';
 import {BrowserRouter} from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+   <ThemeProvider theme={LightTheme}>
+     <BrowserRouter>
     <AppRoutes/>
     </BrowserRouter>
+   </ThemeProvider>
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import {Button} from '@mui/material'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/pagina-inicial' element={<p>Página inicial</p>} />
+        <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' >teste</Button>} />
 
         <Route path='*' element={<Navigate to='/pagina-inicial'/>} />
     </Routes>
