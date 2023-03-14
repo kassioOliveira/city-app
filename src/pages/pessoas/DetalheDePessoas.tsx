69e494d992041fca955a69655b50bab6eb047256
kassio oliveira <kassiooliveira875@gmail.com>
2023-03-14 20:31:56 -0300

    Configurando grid layout no formulario e logica do react hook customizado

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,16 +1,26 @@
 
 import { useEffect,useState } from 'react';
 
-import { LinearProgress } from '@mui/material';
+import { Grid, LinearProgress, Paper, Typography } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { FerramentasDeDetalhe } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { VTextField, VForm, useVForm } from '../../shared/forms';
+import { Box } from '@mui/system';
+
+interface IFormData {
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
+
+  const { formRef, save, saveAndClose, isSaveAndClose  } = useVForm();
 
   const [isLoading,setIsLoading] = useState(false);
   const [nome,setNome] = useState('');
@@ -27,15 +37,55 @@ export const DetalheDePessoas: React.FC = () => {
           }else{
             setNome(result.nomeCompleto);
             setIsLoading(false);
-            console.log(result);
+            formRef.current?.setData(result);
           }
         });
+    } else {
+      formRef.current?.setData({
+        email: '',
+        cidadeId: '',
+        nomeCompleto: '',
+      });
     }
 
   }, [id,setNome]);
 
-  const handleSave = () => {
-    console.log('save');
+  const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+
+    if (id === 'nova') {
+      PessoasService
+        .create(dados)
+        .then((result) => {
+          setIsLoading(false);
+
+          if(result instanceof Error){
+            alert(result.message);
+          }else{
+
+            if(isSaveAndClose()){
+              navigate('/pessoas');
+            }else{
+              navigate(`/pessoas/detalhe/${result}`);
+            }
+            
+          }
+        });
+    }else{
+      PessoasService
+        .updateById(Number(id),{id: Number(id), ...dados})
+        .then((result) => {
+          setIsLoading(false);
+
+          if(result instanceof Error){
+            alert(result.message);
+          }else {
+            if(isSaveAndClose()){
+              navigate('/pessoas');
+            }
+          }
+        });
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -64,8 +114,8 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoNovo={id !== 'nova'}
           mostrarBotaoApagar={id !== 'nova'}
 
-          aoClicarEmSalvar={handleSave}
-          aoClicarEmSalvarEVoltar={handleSave}
+          aoClicarEmSalvar={save}
+          aoClicarEmSalvarEVoltar={saveAndClose}
           aoClicarEmApagar={()=> handleDelete(Number(id))}
           aoClicarEmVoltar={()=> navigate('/pessoas')}
           aoClicarEmNovo={()=> navigate('/pessoas/detalhe/nova')}
@@ -73,12 +123,69 @@ export const DetalheDePessoas: React.FC = () => {
         />
       }
     >
-      {
-        isLoading && (
-          <LinearProgress variant='indeterminate'/>
-        )
-      }
-      <div>DetalheDePessoas {id}</div>
+      
+      <VForm ref={formRef} onSubmit={handleSave}>
+        <Box margin={1} display='flex' flexDirection='column' component={Paper} variant='outlined'>
+
+          <Grid container direction='column' padding={2} spacing={2}>
+
+            {isLoading && (
+              <Grid item>
+                <LinearProgress variant='indeterminate'/>
+              </Grid>
+            )}
+
+            <Grid item>
+              <Typography variant='h6'>Geral</Typography> 
+            </Grid>
+
+            <Grid container item direction='row' spacing={2}>
+
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  fullWidth
+                  label='Nome completo'
+                  disabled={isLoading}
+                  name='nomeCompleto'
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </Grid>
+        
+            </Grid>
+
+            <Grid container item direction='row' spacing={2}>
+
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  label='Email'
+                  fullWidth
+                  disabled={isLoading}
+                  name='email'
+                />
+              </Grid>
+        
+            </Grid>
+
+            <Grid container item direction='row' spacing={2}>
+
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                <VTextField
+                  label='Cidade'
+                  fullWidth
+                  disabled={isLoading}
+                  name='cidadeId'
+                />
+              </Grid>
+        
+            </Grid>
+            
+        
+          </Grid>
+
+        </Box>
+        
+      </VForm>
+
     </LayoutBaseDePagina>
   );
 };
